1620f2d7b308b388f4d78efc8f96a5bfb9fc91dd
DearYuto <reasons@kakao.com>
2024-04-04 02:38:21 +0900

    chore: config 파일 수정

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -20,7 +20,7 @@ const config: Config = {
   organizationName: 'DearYuto', // Usually your GitHub org/user name.
   projectName: 'DearYuto.github.io', // Usually your repo name.
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
